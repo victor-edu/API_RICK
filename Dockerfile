bd91161ce2037b52ade650b94ea1b9838b862d1e
victor eduardo de freire marques <victor.marques@cofen.gov.br>
2023-09-12 13:31:11 -0300

    configuração

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,11 @@
 FROM node:18
 
-WORKDIR /user/app
-
-COPY package.json ./
-
-RUN npm install
+WORKDIR /usr/app
 
 COPY . .
 
+RUN npm install 
+
 EXPOSE 3333
 
-CMD ['npm', 'run', 'dev']
+CMD ["node", "server.js"]
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema');
+const cors = require('cors');
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
   schema,
-  graphiql: true, 
+  graphiql: true,
 }));
 
 const PORT = process.env.PORT || 3333;
